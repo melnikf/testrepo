80ff29bff1476c513eb21bb0734f877da301a0df
Aleksandr Melnik <melnik.festu@ya.ru>
2015-09-13 10:56:45 +1000

    First commit

---
+++ melnik-oop/pract-1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
